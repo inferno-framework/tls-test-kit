8258cc7a05477384b2e5396b1040cae2042bc110
Leon Dhoska <LeonDhoska@gmail.com>
2026-05-11 14:27:13 -0400

    Release 1.0.1 (#29)

--- lib/tls_test_kit/version.rb
+++ lib/tls_test_kit/version.rb
@@ -1,6 +1,6 @@
 # version.rb
 
 module TLSTestKit
-  VERSION = '1.0.0'.freeze
-  LAST_UPDATED = '2025-07-21'.freeze # TODO update next release
+  VERSION = '1.0.1'.freeze
+  LAST_UPDATED = '2026-05-11'.freeze # TODO update next release
 end
